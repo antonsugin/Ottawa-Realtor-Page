7b4df919028d0e42c58db964f139633dcb48b538
Emma Paris <suginant@gmail.com>
2020-11-21 16:09:20 +0200

    modified css, html and js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function listedDate (curDate) {
     return curDate.getDate() + ' ' +  month + ' ' + curDate.getFullYear()
 }
 
-
+// Set current date
 function setDate () {
 
     var date = document.getElementsByClassName('house-description__date-current');
@@ -29,24 +29,6 @@ function setOpenHouseDate () {
 
 setDate();
 setOpenHouseDate ();
-
-// var day = now.getDate();
-// var curDay = day + 22;
-// console.log(curDay);
-
-
-// var changeColor = document.getElementById("name"); 
-//         changeColor.onchange = function (e) { 
-//             if (changeColor.value !== '') { 
-//                 e.target.style.backgroundColor  
-//                         = " green";
-//                 e.target.style.color  
-//                 = "white"; 
-//             } 
-//         }; 
-
-// var kony = document.getElementsByTagName("input")
-// console.log(kony)
 
 
 
@@ -72,149 +54,53 @@ function topFunction() {
 }
 
 
-
-
-
-
-var kony = document.getElementById("name");
-
-kony.onchange = function (e) { 
-    if (kony.value !== '') { 
-        // e.target.style.backgroundColor  = " chartreuse";
-        // e.target.style.color = "blue";
-        e.target.style.color = "white";
-        e.target.style.background = 'linear-gradient(to top, #11998e, #38ef7d)'
-    } 
-
-    else {
-        // e.target.style.backgroundColor  = "white";
-        e.target.style.background  = "white";
-    }
-}; 
-
-// background: linear-gradient(to top, #11998e, #38ef7d);
-
-var lony = document.getElementById("property");
-
-lony.onchange = function (e) { 
-    if (lony.value !== '' && lony.value !== 'other') { 
-        e.target.style.backgroundColor  = "chartreuse";
-        e.target.style.color = "blue"
-    } 
-
-    else {
-        e.target.style.backgroundColor  = "white";
-        e.target.style.color = "#4F545F"
-    }
-}; 
-
-
-
-
-var dony = document.getElementById("email");
-
-dony.onchange = function (e) { 
-    if (dony.value !== '') { 
-        e.target.style.backgroundColor  = " chartreuse";
-        e.target.style.color = "blue"
-    } 
-
-    else {
-        e.target.style.backgroundColor  = "white";
-    }
-}; 
-
-
-function changeInputColor (id) {
-    
-    // console.log(id)
-    
+// Change input colors when filled up
+function changeInputColor (id, type) {
         
-        var pony = document.getElementById(id);
+    var curInput = document.getElementById(id);
 
-        pony.onchange = function (e) { 
-            if (pony.value !== '') { 
-                e.target.style.backgroundColor  = " chartreuse";
-                e.target.style.color = "blue"
-            } 
+    curInput.onchange = function (e) { 
 
-            else {
-                e.target.style.background  = "white";
+        if (type === 'email' || type === 'name') {
+
+            if (type === 'email' && curInput.value.includes('@') && curInput.value.includes('.')) {
+                e.target.style.backgroundColor  = "limegreen";
+                e.target.style.color = "whitesmoke"
+                curInput.classList.remove('required');
+                
             }
-        }; 
-    } 
+            else if (type === 'name' && curInput.value !== '' ) {
+                e.target.style.backgroundColor  = "limegreen";
+                e.target.style.color = "whitesmoke"
+            } 
+            else {
+                curInput.classList.add('required');
+                e.target.style.backgroundColor  = "white";
+                e.target.style.color = "black"
+            }
+        }
+
+        else if (curInput.value !== '' && curInput.value !== 'other') { 
+            e.target.style.backgroundColor  = "limegreen";
+            e.target.style.color = "whitesmoke"
+        } 
+        else {
+            e.target.style.background  = "white";
+            e.target.style.color  = "#4F545F";
+        }
+    }; 
+} 
 
 
-
-
-
-
-
-
-
-
-
-// var allInputs = document.getElementsByTagName("input");
-// console.log(allInputs[2].id)
-
-// var ghj = document.getElementById(allInputs[2].id);
-// console.log(ghj)
-
-// ghj.onchange = function (e) { 
-//     if (ghj.value !== '') { 
-//         e.target.style.backgroundColor  
-//                 = " chartreuse";
-//     } 
-
-//     else {
-//         e.target.style.backgroundColor  
-//                 = "white";
-//     }
-// }; 
-
-// var allInputs = document.getElementsByTagName("input");
-
-
-    // else {
-    //     e.target.style.backgroundColor  
-    //             = "white";
-    // }
-    // console.log(kony)
-    // kony.onchange = function (e) { 
-    //                 if (kony.value !== '') { 
-    //                     e.target.style.backgroundColor  
-    //                             = " chartreuse";
-                        
-    //                 } 
-
-    //                 else {
-    //                     e.target.style.backgroundColor  
-    //                             = "white";
-    //                 }
-    // }; 
-    
-// }
-
-
-// for (var i = 0; allInputs.length > 0; allInputs++) {
-//     var ghj = document.getElementById(allInputs[i].id);
-//     ghj.onchange = function (e) { 
-//         if (ghj.value !== '') { 
-//             e.target.style.backgroundColor  
-//                     = " chartreuse";
-//         } 
-
-//         else {
-//             e.target.style.backgroundColor  
-//                     = "white";
-//         }
-//     }; 
-// }
-
-// changeInputColor ();
-
-// if (kony.value !== '' ) {
-//     kony.classList.add("chan-color");
-// }
-
+// function isValid (curInp) {
+//     if (type === 'email') {
+//         e.target.style.backgroundColor  = "blue";
+//         e.target.style.color = "whitesmoke"
         
+//     }
+//     else {
+//         e.target.style.border = "1px solid red"
+//         e.target.style.background  = "white";
+//         e.target.style.color  = "#4F545F";
+//     }
+// } 
